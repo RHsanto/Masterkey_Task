0976625e437ee3948422768440e4545d2108a5c0
RHsanto <rakibulsanto789@gmail.com>
2022-05-30 01:55:33 +0600

    [done navbar section]

--- src/page/common/Navbar.js
+++ src/page/common/Navbar.js
@@ -1,10 +1,39 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
+import './all.css'
 const Navbar = () => {
   return (
     <div>
-      <h1>Navbar
-Navbar</h1>
+    <div className="container">
+    <div className="menu-bar">
+      <div className="top-menu  d-lg-flex justify-content-between py-4">
+        <div className="logo">
+          <h1>LOGO</h1>
+        </div>
+        <div className="language">
+          <p>BN / EN</p>
+        </div>
+      </div>
+     <div className=" d-lg-flex justify-content-between align-items-center">
+       <nav>
+         <>
+          <NavLink to='/'><li className='active'>HOME</li></NavLink>
+          <NavLink to='#'><li>ADVERTISERS</li></NavLink>
+          <NavLink to='#'><li>PUBLISHERS</li></NavLink>
+          <NavLink to='#'><li>INFLUENCER</li></NavLink>
+          <NavLink to='#'><li>AD FORMATS</li></NavLink>
+          <NavLink to='#'><li>BLOG</li></NavLink>
+          <NavLink to='#'><li>CONTACT US</li></NavLink>
+         </>
+       </nav>
+
+       <div className="auth shadow ">
+         <button className='login-btn'>LOGIN </button>
+         <button className='signUp-btn shadow'>SIGN UP</button>
+       </div>
+     </div>
+     </div>
+    </div>
     </div>
   );
 };
