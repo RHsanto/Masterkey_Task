9e1358f9b7fc616aa1c37db1db72dba4bd90cf53
RHsanto <rakibulsanto789@gmail.com>
2022-05-30 00:48:34 +0600

    [add] initial set up

---
+++ src/page/Home/AD_Section/AD.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AD = () => {
+  return (
+    <div>
+      <h1>AD
+AD</h1>
+    </div>
+  );
+};
+
+export default AD;
---
+++ src/page/Home/Sliders/Sliders.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sliders = () => {
+  return (
+    <div>
+      <h1>Sliders
+Sliders</h1>
+    </div>
+  );
+};
+
+export default Sliders;
