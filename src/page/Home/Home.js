74a3d4e60055dfa397063fc8e08a2c4a285dffc4
RHsanto <rakibulsanto789@gmail.com>
2022-05-30 00:55:46 +0600

    [add] initial set up

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Achievement from './Achievement_section/Achivement';
 import AD from './AD_Section/AD';
+import Banner from './Banner';
 import Services from './Services/Services';
 import Sliders from './Sliders/Sliders';
 
 const Home = () => {
   return (
     <div>
+      <Banner/>
       <Services/>
       <AD/>
       <Achievement/>
