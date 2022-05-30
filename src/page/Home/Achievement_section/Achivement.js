b5162a7da001e3b137b13c7f0b6f18e5e445745a
RHsanto <rakibulsanto789@gmail.com>
2022-05-30 13:07:23 +0600

    [ADD] start achivement section

--- src/page/Home/Achievement_section/Achivement.js
+++ src/page/Home/Achievement_section/Achivement.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Achievement = () => {
   return (
-    <div>
-      <h1>Achievement</h1>
+    <div className='achievement'>
+     <div className="container">
+     <h2 className='title'>OUR ALL ACHIEVEMENTS </h2>
+     <div className="row">
+       <div className="col-lg-4">1</div>
+       <div className="col-lg-4">2</div>
+       <div className="col-lg-4">3</div>
+     </div>
+     </div>
     </div>
   );
 };
