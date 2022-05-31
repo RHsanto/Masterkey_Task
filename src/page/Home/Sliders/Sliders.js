a3a893c314856530f53be774bbaab8220ee083ff
RHsanto <rakibulsanto789@gmail.com>
2022-05-31 12:08:52 +0600

    add

--- src/page/Home/Sliders/Sliders.js
+++ src/page/Home/Sliders/Sliders.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Rating from 'react-rating';
 import Slider from "react-slick";
 const Sliders = () => {
   var settings = {
@@ -10,65 +11,35 @@ const Sliders = () => {
     pauseOnHover: true,
     
   };
+  const [sliders,setSliders]=useState([]);
+  useEffect(()=>{
+    fetch('./slider.json')
+    .then(res=>res.json())
+    .then(data=>setSliders(data))
+  },[])
   return (
     <div className='sliders'>
       <div className="container">
         <h2 className='title'>OUR HAPPY CUSTOMER </h2>
         <Slider {...settings} >
-          <div className='slide  d-flex justify-content-center align-items-center'>
-           <div className="slide-img">
-             <img src="https://learning-management-system-theta.vercel.app/static/media/learning-2.d1759e5361aaa4a0e5bd.png" alt="" />
+          {sliders?.map(data=>
+            <div className='slide  d-flex justify-content-center '>
+            <div className="slide-img">
+              <img src={data?.img} alt="" />
+            </div>
+            <div className="slide-info p-3">
+              <h2>{data?.name}</h2>
+              <p>{data?.reviews}</p>
+              <Rating 
+                emptySymbol="far fa-star text-warning"
+                fullSymbol="fas fa-star text-warning"
+                readonly
+                initialRating={data?.ratings}> 
+              </Rating>
+            </div>
            </div>
-           <div className="slide-info">
-             <h2>Do You Want to Learn here?</h2>
-             <p>Dramatically supply transparent  deliverables before & can backward comp internet or organic sources.</p>
-           </div>
-          </div>
-          <div className='slide d-flex justify-content-center align-items-center'>
-           <div className="slide-img">
-             <img src="https://learning-management-system-theta.vercel.app/static/media/madam-3.cecfbd9d3246c3fe119d.png" alt="" />
-           </div>
-           <div className="slide-info">
-             <h2>Do You Want to Learn here?</h2>
-             <p>Dramatically supply transparent deliverables before & you.</p>
-           </div>
-          </div>
-          <div className='slide  d-flex justify-content-center align-items-center'>
-           <div className="slide-img">
-             <img src="https://learning-management-system-theta.vercel.app/static/media/learning-2.d1759e5361aaa4a0e5bd.png" alt="" />
-           </div>
-           <div className="slide-info">
-             <h2>Do You Want to Learn here?</h2>
-             <p>Dramatically supply transparent deliverables before & you.</p>
-           </div>
-          </div>
-          <div className='slide d-flex justify-content-center align-items-center'>
-           <div className="slide-img">
-             <img src="https://learning-management-system-theta.vercel.app/static/media/madam-3.cecfbd9d3246c3fe119d.png" alt="" />
-           </div>
-           <div className="slide-info">
-             <h2>Do You Want to Learn here?</h2>
-             <p>Dramatically supply transparent deliverables before & you.</p>
-           </div>
-          </div>
-          <div className='slide  d-flex justify-content-center align-items-center'>
-           <div className="slide-img">
-             <img src="https://learning-management-system-theta.vercel.app/static/media/learning-2.d1759e5361aaa4a0e5bd.png" alt="" />
-           </div>
-           <div className="slide-info">
-             <h2>Do You Want to Learn here?</h2>
-             <p>Dramatically supply transparent deliverables before & you.</p>
-           </div>
-          </div>
-          <div className='slide d-flex justify-content-center align-items-center'>
-           <div className="slide-img">
-             <img src="https://learning-management-system-theta.vercel.app/static/media/madam-3.cecfbd9d3246c3fe119d.png" alt="" />
-           </div>
-           <div className="slide-info">
-             <h2>Do You Want to Learn here?</h2>
-             <p>Dramatically supply transparent deliverables before & you.</p>
-           </div>
-          </div>
+            )}
+        
         </Slider>
       </div>
     </div>
